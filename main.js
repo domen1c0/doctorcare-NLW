74200203174f6045f79fd1b6b24dfe1338511739
Paolo Domenico <paolodomenico777@gmail.com>
2022-05-06 18:46:15 -0300

    Finish

---
+++ main.js
@@ -0,0 +1,66 @@
+window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackToTopButtonOnScroll()
+  activateMenuAtCurrentSection(home)
+  activateMenuAtCurrentSection(services)
+  activateMenuAtCurrentSection(about)
+}
+
+function activateMenuAtCurrentSection(section){
+  const targetLine = scrollY + innerHeight / 2;
+
+  const sectionTop = section.offsetTop
+  const sectionHeight = section.offsetHeight
+  const sectionTopReachOrPassedTargetLine = targetLine >= sectionTop
+
+  const sectionEndsAt = sectionTop + sectionHeight
+  const sectionEndPassedTargetLine = sectionEndsAt <= targetLine
+
+  const sectionBoundaries = sectionTopReachOrPassedTargetLine && !sectionEndPassedTargetLine
+
+  const sectionId = section.getAttribute('id')
+  const menuElement = document.querySelector(`.menu a[href*=${sectionId}]`)
+
+  menuElement.classList.remove('active')
+  if(sectionBoundaries){
+    menuElement.classList.add('active')
+  }
+
+}
+
+
+function showNavOnScroll() {
+  if (scrollY > 0) {
+    navigation.classList.add('scroll')
+  } else {
+    navigation.classList.remove('scroll')
+  }
+}
+
+function showBackToTopButtonOnScroll() {
+  if (scrollY > 550) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+function openMenu() {
+  document.body.classList.add('menu-expanded')
+}
+function closeMenu() {
+  document.body.classList.remove('menu-expanded')
+}
+
+let slideUp = {
+  origin: 'top',
+  distance: '30px',
+  duration: 700
+}
+
+ScrollReveal(slideUp).reveal(
+  '#home, #home img, #home .stats, #services, #services header, #services .cards'
+)
